c811f872534dc1ae2eb0ec9b55985b1365bde5f8
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-05-09 13:29:44 -0500

    chore: remove boilerplate and update README

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import Nav from '../components/nav'
-
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
-      <div className="hero">
-        <h1 className="title">Next.js + Tailwind CSS</h1>
-      </div>
+      <h1>My Blog</h1>
     </div>
-  )
+  );
 }
