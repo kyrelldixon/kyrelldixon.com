f7abfa5a73b37ef7442a2ce2be567d353a8c9bf0
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-05-10 14:42:49 -0500

    feat: dynamically generate blog post page

--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -1,13 +1,39 @@
-export default function BlogPost() {
+import { getAllPostPaths, getPostData } from "../../lib/posts";
+
+export default function BlogPost({ postData }) {
+  console.log({ postData });
   return (
     <div>
       <article className="max-w-2xl mx-auto">
         <header className="text-center">
-          <h1 className="font-semibold text-7xl">Post Title</h1>
-          <p className="text-2xl">Description</p>
+          <h1 className="mb-10 text-5xl font-semibold leading-tight md:text-6xl lg:text-7xl xl:text-8xl">
+            {postData.title}
+          </h1>
+          <h2 className="mb-6 text-2xl leading-snug">{postData.excerpt}</h2>
         </header>
-        <main></main>
+        <main
+          className="space-y-4"
+          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+        ></main>
       </article>
     </div>
   );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostPaths();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.post);
+
+  return {
+    props: {
+      postData,
+    },
+  };
 }
