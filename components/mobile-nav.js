ab5cf7facefb7aa9f0c3e1c345cd2010c0fb8042
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-05-11 10:53:40 -0500

    feat: add mobile-nav

---
+++ components/mobile-nav.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav className="fixed inset-x-0 bottom-0 w-full bg-white md:hidden">
+      <ul className="flex items-end justify-around h-full px-2 py-2 text-sm font-semibold">
+        <li>
+          <Link href="/blog">
+            <a>Articles</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/coaching">
+            <a>Coaching</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/newsletter">
+            <a>Newsletter</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
