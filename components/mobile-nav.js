946befae590fbbf3bcce55812d2d93cf84ac6766
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-05-11 10:58:11 -0500

    refactor: rename Nav to MobileNav

--- components/mobile-nav.js
+++ components/mobile-nav.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Nav() {
+export default function MobileNav() {
   return (
     <nav className="fixed inset-x-0 bottom-0 w-full bg-white md:hidden">
       <ul className="flex items-end justify-around h-full px-2 py-2 text-sm font-semibold">
