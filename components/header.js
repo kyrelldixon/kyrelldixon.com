9016399376f78cc8fc323639da895b434a955cc9
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-05-11 11:26:53 -0500

    feat: add header and nav links

---
+++ components/header.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export default function Header({ navLinks }) {
+  return (
+    <header className="sticky inset-x-0 top-0 flex items-center justify-between p-4 bg-white">
+      <Link href="/">
+        <a title="Go to Homepage">
+          <h1 className="text-lg font-semibold">Kyrell Dixon</h1>
+        </a>
+      </Link>
+      <nav>
+        <ul className="hidden space-x-8 md:flex">
+          {navLinks.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href}>
+                <a>{link.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Link href="/coaching">
+        <a className="px-4 py-2 text-sm text-white bg-gray-500 rounded">
+          Work With Me
+        </a>
+      </Link>
+    </header>
+  );
+}
--- components/mobile-nav.js
+++ components/mobile-nav.js
@@ -1,34 +1,16 @@
 import Link from "next/link";
 
-export default function MobileNav() {
+export default function MobileNav({ navLinks }) {
   return (
     <nav className="fixed inset-x-0 bottom-0 w-full bg-white md:hidden">
       <ul className="flex items-end justify-around h-full px-2 py-2 text-sm font-semibold">
-        <li>
-          <Link href="/blog">
-            <a>Articles</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/coaching">
-            <a>Coaching</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/newsletter">
-            <a>Newsletter</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
-        </li>
+        {navLinks.map((link) => (
+          <li key={link.href}>
+            <Link href={link.href}>
+              <a>{link.title}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
