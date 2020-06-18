07a2200ecaeabbb7670953774fec380175bf651e
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-06-18 16:14:58 -0500

    feat: add mobile nav

--- components/page-layout.js
+++ components/page-layout.js
@@ -1,14 +1,16 @@
 import Header from "./header";
 import Alert from "./alert";
+import MobileNav from "./mobile-nav";
 
-const navLinks = [];
+const navLinks = [{ title: "Articles", href: "/blog", icon: "blog" }];
 
 export default function PageLayout({ children, preview = false }) {
   return (
-    <div className="text-white bg-black">
+    <div className="pb-8 text-white bg-black md:pb-0">
       {preview && <Alert preview={preview} />}
       <Header navLinks={navLinks} />
       {children}
+      <MobileNav navLinks={navLinks} />
     </div>
   );
 }
