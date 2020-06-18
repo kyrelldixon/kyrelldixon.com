a54403c1ece73287f40a55343e935b85d1996397
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-06-18 15:57:27 -0500

    feat: add blog to header

--- components/header.js
+++ components/header.js
@@ -2,12 +2,23 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="inset-x-0 top-0 z-10 flex items-center justify-center p-4 bg-black ">
-      <Link href="/">
-        <a title="Go to Homepage">
-          <h1 className="text-lg font-semibold">Kyrell Dixon</h1>
-        </a>
-      </Link>
+    <header className="inset-x-0 top-0 z-10 px-8 py-8 bg-black">
+      <div className="flex items-center justify-between max-w-4xl mx-auto">
+        <Link href="/">
+          <a title="Go to Homepage">
+            <h1 className="text-xl font-bold">Kyrell Dixon</h1>
+          </a>
+        </Link>
+        <nav className="">
+          <ul className="hidden space-x-6 text-sm font-semibold uppercase md:flex">
+            <li>
+              <Link href="/">
+                <a>Blog</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
