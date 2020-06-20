42228e3a7b2beda6079324d6ec4c58cbc7250f61
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-06-20 01:51:32 -0500

    feat: add who is this for section

--- pages/coaching.js
+++ pages/coaching.js
@@ -4,7 +4,7 @@ export default function CoachingPage() {
   return (
     <PageLayout>
       <main className="p-4 md:mx-auto">
-        <section className="flex flex-col items-center max-w-4xl py-8 text-center md:mx-auto md:py-12">
+        <section className="flex flex-col items-center max-w-4xl py-12 text-center md:mx-auto md:py-12">
           <h1 className="mb-6 text-5xl font-extrabold leading-tight md:text-6xl lg:text-7xl">
             I'm here to help
           </h1>
@@ -21,12 +21,12 @@ export default function CoachingPage() {
             Schedule a Session
           </a>
         </section>
-        <section className="py-8 md:py-12">
+        <section className="py-16 md:py-20">
           <div className="max-w-2xl mx-auto">
-            <h2 className="mb-10 text-3xl font-extrabold leading-tight md:mb-6 md:text-5xl">
+            <h2 className="mb-10 text-2xl font-extrabold leading-tight md:mb-10 md:text-4xl">
               What to expect
             </h2>
-            <div className="space-y-6 leading-relaxed">
+            <div className="space-y-4 leading-relaxed">
               <p>
                 On a one-to-one basis I take a flexible teaching approach to
                 teaching coding concepts on{" "}
@@ -48,8 +48,8 @@ export default function CoachingPage() {
             </div>
           </div>
           <div className="grid max-w-4xl grid-cols-1 gap-8 mx-auto mt-12 md:grid-cols-2 md:grid-rows-2">
-            <div className="p-4 bg-blue-600 rounded-lg md:p-6 lg:p-8">
-              <h3 className="mb-4 text-xl font-extrabold md:text-2xl">
+            <div className="p-4 bg-gray-900 rounded-lg shadow md:p-6 lg:p-8">
+              <h3 className="mb-4 text-lg font-extrabold md:text-2xl">
                 1. Understanding
               </h3>
               <p>
@@ -58,8 +58,8 @@ export default function CoachingPage() {
                 me to deliver specific actionable advice for your development.
               </p>
             </div>
-            <div className="p-4 bg-blue-600 rounded-lg md:p-6 lg:p-8">
-              <h3 className="mb-4 text-xl font-extrabold md:text-2xl">
+            <div className="p-4 bg-gray-900 rounded-lg shadow md:p-6 lg:p-8">
+              <h3 className="mb-4 text-lg font-extrabold md:text-2xl">
                 2. Direction
               </h3>
               <p>
@@ -68,7 +68,7 @@ export default function CoachingPage() {
                 success and provide resources for you to learn from.
               </p>
             </div>
-            <div className="p-4 bg-blue-600 rounded-lg md:p-6 lg:p-8">
+            <div className="p-4 bg-gray-900 rounded-lg shadow md:p-6 lg:p-8">
               <h3 className="mb-4 text-xl font-extrabold md:text-2xl">
                 3. Support
               </h3>
@@ -78,7 +78,7 @@ export default function CoachingPage() {
                 can be confident moving forward with me on your team.
               </p>
             </div>
-            <div className="p-4 bg-blue-600 rounded-lg md:p-6 lg:p-8">
+            <div className="p-4 bg-gray-900 rounded-lg shadow md:p-6 lg:p-8">
               <h3 className="mb-4 text-xl font-extrabold md:text-2xl">
                 4. Success
               </h3>
@@ -92,21 +92,53 @@ export default function CoachingPage() {
         </section>
         <section className="py-8 md:py-12">
           <div className="max-w-2xl mx-auto">
-            <h2 className="mb-4 text-3xl font-extrabold leading-tight md:mb-6 md:text-5xl">
-              What you can learn
+            <h2 className="mb-10 text-2xl font-extrabold leading-tight md:mb-10 md:text-4xl">
+              What you will learn
             </h2>
           </div>
         </section>
         <section className="py-8 md:py-12">
           <div className="max-w-2xl mx-auto">
-            <h2 className="mb-4 text-3xl font-extrabold leading-tight md:mb-6 md:text-5xl">
-              Who this is for
+            <h2 className="mb-10 text-2xl font-extrabold leading-tight md:mb-10 md:text-4xl">
+              Who is this for
             </h2>
+            <div className="leading-relaxed">
+              <p className="mb-8">
+                While I love to help people, I'm not for everyone. You should
+                reach out if:
+              </p>
+              <ul className="mb-8 space-y-4 list-disc list-inside">
+                <li>
+                  You are enrolled in a Web Development bootcamp and feel like
+                  you are getting behind
+                </li>
+                <li>
+                  You are taking an online programming class and struggling
+                  through assignments alone
+                </li>
+                <li>
+                  You are changing careers and trying to pick up some coding
+                  skills
+                </li>
+                <li>
+                  You are a designer or developer and getting skills to make
+                  progress in your career
+                </li>
+                <li>
+                  You are business-minded and looking to make money from code
+                </li>
+                <li>You are a looking for guidance and support as you learn</li>
+                <li>You are stuck on a problem and just need some help</li>
+              </ul>
+            </div>
+            <p>
+              If at least one of these sounds like you, we will be a great fit.
+            </p>
           </div>
         </section>
         <section className="py-8 md:py-12">
           <div className="max-w-2xl mx-auto">
-            <h2 className="mb-4 text-3xl font-extrabold leading-tight md:mb-6 md:text-5xl">
+            <h2 className="mb-10 text-2xl font-extrabold leading-tight md:mb-10 md:text-4xl">
               What other people say
             </h2>
           </div>
