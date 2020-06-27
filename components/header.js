77e381f7f75acffa48185691a93e2028ba0336fe
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-06-26 23:48:33 -0500

    feat: active nav link styling

--- components/header.js
+++ components/header.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header({ navLinks }) {
+  const router = useRouter();
+
   return (
     <header className="inset-x-0 top-0 z-10 px-8 py-8 bg-black">
       <div className="flex items-center justify-center max-w-4xl mx-auto text-center md:justify-between md:text-left">
@@ -13,7 +16,9 @@ export default function Header({ navLinks }) {
           <ul className="hidden space-x-6 text-sm font-semibold md:flex">
             {navLinks.map(({ title, href }, i) => (
               <li
-                className="transition duration-200 ease-in-out opacity-75 hover:opacity-100"
+                className={`transition duration-200 ease-in-out ${
+                  router.pathname === href ? "" : "opacity-75 hover:opacity-100"
+                }`}
                 key={i}
               >
                 <Link href={href}>
