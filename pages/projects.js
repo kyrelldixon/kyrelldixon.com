af42906f46846498cff4622a66252a08c87232d2
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-08-16 08:44:15 -0500

    refactor: make project component

--- pages/projects.js
+++ pages/projects.js
@@ -12,146 +12,69 @@ export default function Projects() {
           </h1>
         </section>
         <section className="grid max-w-3xl grid-flow-row grid-cols-1 gap-4 py-8 mx-auto md:gap-8 md:grid-cols-2">
-          <div className="p-8 bg-gray-900 rounded-lg shadow-md light:bg-gray-100">
-            <h3 className="text-lg font-extrabold">Write More App</h3>
-            <div className="flex mt-4">
-              <Link
-                className="flex items-center mr-8 transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://write-more.now.sh/"
-              >
-                <span className="mr-1">
-                  <Icon name="external-link" />
-                </span>
-                Demo
-              </Link>
-              <Link
-                className="flex items-center transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://github.com/kyrelldixon/write-more"
-              >
-                <span className="mr-1">
-                  <Icon name="github" />
-                </span>
-                Source Code
-              </Link>
-            </div>
-          </div>
-          <div className="p-8 bg-gray-900 rounded-lg shadow-md light:bg-gray-100">
-            <h3 className="text-lg font-extrabold">Huddle Landing Page</h3>
-            <div className="flex mt-4">
-              <Link
-                className="flex items-center mr-8 transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://huddle-landing-demo.now.sh/"
-                target="_blank"
-              >
-                <span className="mr-1">
-                  <Icon name="external-link" />
-                </span>
-                Demo
-              </Link>
-              <Link
-                className="flex items-center transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://github.com/kyrelldixon/huddle-landing-page"
-                target="_blank"
-              >
-                <span className="mr-1">
-                  <Icon name="github" />
-                </span>
-                Source Code
-              </Link>
-            </div>
-          </div>
-          <div className="p-8 bg-gray-900 rounded-lg shadow-md light:bg-gray-100">
-            <h3 className="text-lg font-extrabold">Lonely Planet</h3>
-            <div className="flex mt-4">
-              <Link
-                className="flex items-center mr-8 transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://lonely-planet.now.sh/"
-                target="_blank"
-              >
-                <span className="mr-1">
-                  <Icon name="external-link" />
-                </span>
-                Demo
-              </Link>
-              <Link
-                className="flex items-center transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://github.com/kyrelldixon/lonely-planet"
-                target="_blank"
-              >
-                <span className="mr-1">
-                  <Icon name="github" />
-                </span>
-                Source Code
-              </Link>
-            </div>
-          </div>
-          <div className="p-8 bg-gray-900 rounded-lg shadow-md light:bg-gray-100">
-            <h3 className="text-lg font-extrabold">Recipe Site Skeleton</h3>
-            <div className="flex mt-4">
-              <Link
-                className="flex items-center mr-8 transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://recipe-skeleton.vercel.app/"
-                target="_blank"
-              >
-                <span className="mr-1">
-                  <Icon name="external-link" />
-                </span>
-                Demo
-              </Link>
-              <Link
-                className="flex items-center transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://github.com/kyrelldixon/recipe"
-                target="_blank"
-              >
-                <span className="mr-1">
-                  <Icon name="github" />
-                </span>
-                Source Code
-              </Link>
-            </div>
-          </div>
-          <div className="p-8 bg-gray-900 rounded-lg shadow-md light:bg-gray-100">
-            <h3 className="text-lg font-extrabold">Core Values</h3>
-            <div className="flex mt-4">
-              <Link
-                className="flex items-center mr-8 transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://values.netlify.app/"
-                target="_blank"
-              >
-                <span className="mr-1">
-                  <Icon name="external-link" />
-                </span>
-                Demo
-              </Link>
-              <Link
-                className="flex items-center transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://github.com/kyrelldixon/values"
-                target="_blank"
-              >
-                <span className="mr-1">
-                  <Icon name="github" />
-                </span>
-                Source Code
-              </Link>
-            </div>
-          </div>
-          <div className="p-8 bg-gray-900 rounded-lg shadow-md light:bg-gray-100">
-            <h3 className="text-lg font-extrabold">Shopify Demo App</h3>
-            <div className="flex mt-4">
-              <Link
-                className="flex items-center transition duration-200 ease-in-out opacity-75 hover:opacity-100"
-                href="https://github.com/kyrelldixon/shopify-demo-app-hooks"
-                target="_blank"
-              >
-                <span className="mr-1">
-                  <Icon name="github" />
-                </span>
-                Source Code
-              </Link>
-            </div>
-          </div>
+          <Project
+            name="Write More App"
+            site="https://write-more.now.sh/"
+            github="https://github.com/kyrelldixon/write-more"
+          />
+          <Project
+            name="Huddle Landing Page"
+            site="https://huddle-landing-demo.now.sh/"
+            github="https://github.com/kyrelldixon/huddle-landing-page"
+          />
+          <Project
+            name="Lonely Planet"
+            site="https://lonely-planet.now.sh/"
+            github="https://github.com/kyrelldixon/lonely-planet"
+          />
+          <Project
+            name="Recipe Site Skeleton"
+            site="https://recipe-skeleton.vercel.app/"
+            github="https://github.com/kyrelldixon/recipe"
+          />
+          <Project
+            name="Core Values"
+            site="https://values.netlify.app/"
+            github="https://github.com/kyrelldixon/values"
+          />
+          <Project
+            name="Shopify Demo App"
+            github="https://github.com/kyrelldixon/shopify-demo-app-hooks"
+          />
         </section>
       </main>
     </PageLayout>
+  );
+}
+
+function Project({ name, site, github }) {
+  return (
+    <div className="p-8 bg-gray-900 rounded-lg shadow-md light:bg-gray-100">
+      <h3 className="text-lg font-extrabold">{name}</h3>
+      <div className="flex mt-4">
+        {site && (
+          <Link
+            className="flex items-center mr-8 transition duration-200 ease-in-out opacity-75 hover:opacity-100"
+            href={site}
+          >
+            <span className="mr-1">
+              <Icon name="external-link" />
+            </span>
+            Demo
+          </Link>
+        )}
+        {github && (
+          <Link
+            className="flex items-center transition duration-200 ease-in-out opacity-75 hover:opacity-100"
+            href={github}
+          >
+            <span className="mr-1">
+              <Icon name="github" />
+            </span>
+            Source Code
+          </Link>
+        )}
+      </div>
+    </div>
   );
 }
