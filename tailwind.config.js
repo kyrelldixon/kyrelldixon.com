4bba2ca39170bd805ea78d72547ce064656b9dc3
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-02-03 08:16:32 -0600

    chore(config): Update Tailwind and postcss config to latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,35 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ["./components/**/*.js", "./pages/**/*.js", "./styles/**/*.js"],
+  purge: ['./components/**/*.js', './pages/**/*.js', './styles/**/*.js'],
+  darkMode: 'media',
   theme: {
     extend: {
-      fontSize: {
-        "7xl": "5rem",
-        "8xl": "6rem",
-      },
       colors: {
-        black: "#181818",
-        gray: {
-          "100": "#f5f5f5",
-          "200": "#eeeeee",
-          "300": "#e0e0e0",
-          "400": "#bdbdbd",
-          "500": "#9e9e9e",
-          "600": "#757575",
-          "700": "#616161",
-          "800": "#424242",
-          "900": "#212121",
-        },
-        "f-blue": "#09f",
-        "f-purple": "#85f",
-        "f-teal": "#0cf",
-        "f-green": "#0c8",
-        "f-yellow": "#fc0",
-        "f-red": "#f36",
+        black: '#181818',
+        'f-blue': '#09f',
+        'f-purple': '#85f',
+        'f-teal': '#0cf',
+        'f-green': '#0c8',
+        'f-yellow': '#fc0',
+        'f-red': '#f36',
       },
       spacing: {
-        "88": "22rem",
+        88: '22rem',
       },
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
-      screens: {
-        light: { raw: "(prefers-color-scheme: light)" },
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
   variants: {
-    opacity: ["responsive", "hover", "focus", "disabled"],
-    borderWidth: ["responsive", "hover", "focus"],
-    cursor: ["disabled"],
+    opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    borderWidth: ['responsive', 'hover', 'focus'],
+    cursor: ['disabled'],
   },
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
-  plugins: [require("@tailwindcss/ui")],
-};
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
