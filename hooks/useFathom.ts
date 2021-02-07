9417d982305b6128e482878891d501b58b1c4ae2
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-02-07 11:53:26 -0600

    feat(fathom): Create and use custom fathom hook

---
+++ hooks/useFathom.ts
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import * as Fathom from "fathom-client";
+import { useRouter } from "next/router";
+
+/**
+ * Adds tracking with Fathom Analytics.
+ */
+export function useFathom() {
+  const router = useRouter();
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load(process.env.FATHOM_SITE_ID, {
+      includedDomains: [process.env.FATHOM_CUSTOM_URL],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import "styles/index.css";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
+import { useFathom } from "hooks/useFathom";
 
 function MyApp({ Component, pageProps }) {
+  useFathom();
   return (
     <>
       <DefaultSeo {...SEO} />
