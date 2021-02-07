30fba9f2e7bd326a7ae526a1945d2bf9114abae2
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-02-07 11:52:43 -0600

    chore(deps): Install fathom-client

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   env: {
     GHOST_URL: process.env.GHOST_URL,
     GHOST_API_KEY: process.env.GHOST_API_KEY,
+    FATHOM_SITE_ID: process.env.FATHOM_SITE_ID,
+    FATHOM_CUSTOM_URL: process.env.FATHOM_CUSTOM_URL,
   },
 };
